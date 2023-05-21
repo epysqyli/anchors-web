d3be510ea21c0b4c366ee9adebfa9d5f048407ec
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-05-21 15:13:58 +0200

    project: remove boilerplate.

--- src/root.tsx
+++ src/root.tsx
@@ -1,14 +1,22 @@
 // @refresh reload
-import { Routes } from "@solidjs/router";
 import { Suspense } from "solid-js";
-import { Body, FileRoutes, Head, Html, Meta, Scripts, Title } from "solid-start";
+import { Routes } from "@solidjs/router";
 import { ErrorBoundary } from "solid-start/error-boundary";
+import {
+  Body,
+  FileRoutes,
+  Head,
+  Html,
+  Meta,
+  Scripts,
+  Title,
+} from "solid-start";
 
 export default function Root() {
   return (
     <Html lang="en">
       <Head>
-        <Title>SolidStart - With Vitest</Title>
+        <Title>Anchors</Title>
         <Meta charset="utf-8" />
         <Meta name="viewport" content="width=device-width, initial-scale=1" />
       </Head>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,19 +1,10 @@
 import "./index.css";
 import { Component } from "solid-js";
-import Counter from "~/components/Counter";
 
 const Home: Component<{}> = () => {
   return (
     <main>
-      <h1>Hello world!</h1>
-      <Counter />
-      <p>
-        Visit{" "}
-        <a href="https://solidjs.com" target="_blank">
-          solidjs.com
-        </a>{" "}
-        to learn how to build Solid apps.
-      </p>
+      <h1>Anchors</h1>
     </main>
   );
 };
