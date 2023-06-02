5a0c208d0d4bbd64c2ca5f2bcb71ea09e784bfd6
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-02 16:28:55 +0200

    style: remove scrollbar background color and apply break words.

--- src/components/EventWrapper.tsx
+++ src/components/EventWrapper.tsx
@@ -14,7 +14,7 @@ const EventWrapper: Component<Props> = (props) => {
           <For each={props.events}>
             {(nostrEvent) => (
               <div class="snap-start h-[100vh] text-white pt-10 mx-auto w-11/12">
-                <div class="h-[70vh] px-10 text-justify overflow-auto">
+                <div class="h-[70vh] px-10 text-justify overflow-auto break-words">
                   {nostrEvent.content}
                 </div>
               </div>
@@ -24,19 +24,17 @@ const EventWrapper: Component<Props> = (props) => {
       </Show>
 
       <Show when={props.isNarrow !== undefined && !props.isNarrow}>
-          <div class="custom-scrollbar snap-y snap-mandatory overflow-scroll overflow-x-hidden h-full">
-            <For each={props.events}>
-              {(nostrEvent) => (
-                <div
-                  class="snap-start h-full text-white text-xl px-10 pt-10 mx-auto w-4/5 2xl:w-3/5 2xl:p-16 rounded-md"
-                >
-                  <div class="custom-scrollbar h-[70vh] overflow-auto pr-10 text-justify">
-                    {nostrEvent.content}
-                  </div>
+        <div class="custom-scrollbar snap-y snap-mandatory overflow-scroll overflow-x-hidden h-full">
+          <For each={props.events}>
+            {(nostrEvent) => (
+              <div class="snap-start h-full text-white text-xl px-10 pt-10 mx-auto w-4/5 2xl:w-3/5 2xl:p-16 rounded-md">
+                <div class="custom-scrollbar h-[70vh] overflow-auto pr-10 break-words text-justify">
+                  {nostrEvent.content}
                 </div>
-              )}
-            </For>
-          </div>
+              </div>
+            )}
+          </For>
+        </div>
       </Show>
     </>
   );
