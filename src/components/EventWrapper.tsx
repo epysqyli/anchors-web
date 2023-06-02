2c215a1aa248b226aaf713ef1f76e99f840f88f7
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-02 15:13:44 +0200

    event wrapper: adjust height and padding for wide view.

--- src/components/EventWrapper.tsx
+++ src/components/EventWrapper.tsx
@@ -24,11 +24,11 @@ const EventWrapper: Component<Props> = (props) => {
       </Show>
 
       <Show when={props.isNarrow !== undefined && !props.isNarrow}>
-          <div class="custom-scrollbar snap-y snap-mandatory overflow-scroll overflow-x-hidden h-[90vh]">
+          <div class="custom-scrollbar snap-y snap-mandatory overflow-scroll overflow-x-hidden h-full">
             <For each={props.events}>
               {(nostrEvent) => (
                 <div
-                  class="snap-start h-[90vh] text-white text-xl p-10 mx-auto w-4/5 2xl:w-3/5 2xl:p-16 rounded-md"
+                  class="snap-start h-full text-white text-xl px-10 pt-10 mx-auto w-4/5 2xl:w-3/5 2xl:p-16 rounded-md"
                 >
                   <div class="custom-scrollbar h-[70vh] overflow-auto pr-10 text-justify">
                     {nostrEvent.content}
