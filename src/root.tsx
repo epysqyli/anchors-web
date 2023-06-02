e6a1b8a7f7cfb50c3a7f329d2c9dc12050a4bd0e
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-02 15:41:13 +0200

    root: fix mobile chrome bar color.

--- src/root.tsx
+++ src/root.tsx
@@ -33,7 +33,7 @@ const Root: Component<{}> = () => {
           <Title>Anchors</Title>
           <Meta charset="utf-8" />
           <Meta name="viewport" content="width=device-width, initial-scale=1" />
-          <Meta name="theme-color" content="#319197" />
+          <Meta name="theme-color" content="#1e293b" />
         </Head>
         <Body class="h-screen bg-gray-900">
           <Suspense>
