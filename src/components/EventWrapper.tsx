30664ca203bd4a4ca79e480a3872a6a54b499276
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-04 11:17:38 +0200

    index: refactor to use event wrapper for each event.

--- src/components/EventWrapper.tsx
+++ src/components/EventWrapper.tsx
@@ -3,38 +3,18 @@ import { Component, For, Show } from "solid-js";
 
 interface Props {
   isNarrow: boolean | undefined;
-  events: Event[];
+  event: Event;
 }
 
 const EventWrapper: Component<Props> = (props) => {
   return (
     <>
       <Show when={props.isNarrow !== undefined && props.isNarrow}>
-        <div class="snap-y snap-mandatory overflow-scroll overflow-x-hidden h-[100vh]">
-          <For each={props.events}>
-            {(nostrEvent) => (
-              <div class="snap-start h-[100vh] text-white pt-10 mx-auto w-11/12">
-                <div class="h-[70vh] px-10 text-justify overflow-auto break-words">
-                  {nostrEvent.content}
-                </div>
-              </div>
-            )}
-          </For>
-        </div>
+        <></>
       </Show>
 
       <Show when={props.isNarrow !== undefined && !props.isNarrow}>
-        <div class="custom-scrollbar snap-y snap-mandatory overflow-scroll overflow-x-hidden h-full">
-          <For each={props.events}>
-            {(nostrEvent) => (
-              <div class="snap-start h-full text-white text-xl px-10 pt-10 mx-auto w-4/5 2xl:w-3/5 2xl:p-16 rounded-md">
-                <div class="custom-scrollbar h-[70vh] overflow-auto pr-10 break-words text-justify">
-                  {nostrEvent.content}
-                </div>
-              </div>
-            )}
-          </For>
-        </div>
+        <></>
       </Show>
     </>
   );
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,5 +1,12 @@
 import { useIsNarrow } from "~/hooks/useMediaQuery";
-import { Component, createSignal, onMount, useContext } from "solid-js";
+import {
+  Component,
+  For,
+  Show,
+  createSignal,
+  onMount,
+  useContext,
+} from "solid-js";
 import { RelayContext } from "~/contexts/relay";
 import {
   Event,
@@ -43,7 +50,37 @@ const Home: Component<{}> = () => {
     });
   });
 
-  return <EventWrapper isNarrow={isNarrow()} events={events()} />;
+  return (
+    <>
+      <Show when={isNarrow() !== undefined && isNarrow()}>
+        <div class="snap-y snap-mandatory overflow-scroll overflow-x-hidden h-[100vh]">
+          <For each={events()}>
+            {(nostrEvent) => (
+              <div class="snap-start h-[100vh] text-white pt-10 mx-auto w-11/12">
+                <div class="h-[70vh] px-10 text-justify overflow-auto break-words">
+                  {nostrEvent.content}
+                </div>
+              </div>
+            )}
+          </For>
+        </div>
+      </Show>
+
+      <Show when={isNarrow() !== undefined && !isNarrow()}>
+        <div class="custom-scrollbar snap-y snap-mandatory overflow-scroll overflow-x-hidden h-full">
+          <For each={events()}>
+            {(nostrEvent) => (
+              <div class="snap-start h-full text-white text-xl px-10 pt-10 mx-auto w-4/5 2xl:w-3/5 2xl:p-16 rounded-md">
+                <div class="custom-scrollbar h-[70vh] overflow-auto pr-10 break-words text-justify">
+                  {nostrEvent.content}
+                </div>
+              </div>
+            )}
+          </For>
+        </div>
+      </Show>
+    </>
+  );
 };
 
 export default Home;
