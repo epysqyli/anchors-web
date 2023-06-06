cc058ad1558331400a4da591d3a48e9d7ec27fed
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-06 21:40:25 +0200

    narrow-layout: move control button closer to the bottom.

--- src/layouts/NarrowLayout.tsx
+++ src/layouts/NarrowLayout.tsx
@@ -39,7 +39,7 @@ const NarrowLayout: Component<Props> = (props) => {
         transition={{ duration: 0.5 }}
         class="border-4 border-slate-300 rounded-full
           bg-slate-200 cursor-pointer hover:bg-slate-300
-          fixed left-1/2 -translate-x-1/2 bottom-20 select-none
+          fixed left-1/2 -translate-x-1/2 bottom-10 select-none
           active:scale-95 active:border-orange-200 active:bg-orange-50
           transition-transform shadow-lg shadow-slate-900"
         onclick={props.toggleMenu}
