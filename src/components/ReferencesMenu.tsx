a5ffb385c3227e0c0d5363391caa53089391a5d9
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-08 20:51:58 +0200

    reference-menu: create component and add desc.

---
+++ src/components/ReferencesMenu.tsx
@@ -0,0 +1,12 @@
+import { Component } from "solid-js";
+
+interface Props {}
+
+// provides two submenus to manage and add new refs to the event
+// popups clicking VsReferences on write page
+// submenus are snap-x scrollable
+const ReferencesMenu: Component<Props> = (props) => {
+  return <></>;
+};
+
+export default ReferencesMenu;
