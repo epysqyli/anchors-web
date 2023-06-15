437b4cee97d8f79cbec7ee2065e02cf0bb5ff618
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-15 23:11:45 +0200

    openlibrary: return IRefTagResults[] from searchBook.

--- src/lib/open-library.ts
+++ src/lib/open-library.ts
@@ -1,30 +1,35 @@
 import axios from "axios";
+import IRefTagResult from "~/interfaces/IRefTagResult";
 
-interface OpenLibraryResult {
-  key: string;
-  url: string;
-  title: string;
-  cover_i: number;
-  cover_url: string;
-  author_name: string[];
-  first_publish_year: number;
+interface OpenLibraryQueryResult {
+  docs: {
+    key: string;
+    url: string;
+    title: string;
+    cover_i: number;
+    cover_url: string;
+    author_name: string[];
+    first_publish_year: number;
+  }[];
 }
 
-const searchBook = async (searchString: string): Promise<OpenLibraryResult[]> => {
+const searchBook = async (searchString: string): Promise<IRefTagResult[]> => {
   const title = searchString.split(" ").reduce((acc, s) => `${acc}+${s}`);
 
-  const resp = await axios({
+  const resp: OpenLibraryQueryResult = await axios({
     method: "GET",
     url: `https://openlibrary.org/search.json?title=${title}`
   });
 
-  return (resp.data.docs as OpenLibraryResult[]).map((res) => {
-    res.url = `https://openlibrary.org${res.key}`;
-    res.cover_url = `https://covers.openlibrary.org/b/id/${res.cover_i}-S.jpg`;
-    res.author_name = res.author_name == undefined ? [] : res.author_name;
-    res.first_publish_year = res.first_publish_year == undefined ? 0 : res.first_publish_year;
+  return resp.docs.map((res) => {
+    const refTagResult: IRefTagResult = {
+      title: res.title,
+      url: `https://openlibrary.org${res.key}`,
+      creator: res.author_name != undefined && res.author_name.length != 0 ? res.author_name[0] : "",
+      preview: `https://covers.openlibrary.org/b/id/${res.cover_i}-S.jpg`
+    };
 
-    return res;
+    return refTagResult;
   });
 };
 
