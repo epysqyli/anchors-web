985dbc0563ac92fcfc212e0108baaa3abddc2ce9
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-22 23:03:05 +0200

    relay: install websocket polyfill.

--- src/contexts/relay.tsx
+++ src/contexts/relay.tsx
@@ -1,20 +1,16 @@
 import type { Context } from "solid-js";
+import "websocket-polyfill";
 import { Relay, relayInit } from "nostr-tools";
 import { Component, JSX, createContext } from "solid-js";
 
 const relay = relayInit("ws://localhost:2700");
 // const relay = relayInit("wss://nostr.wine");
 (async () => await relay.connect())();
-console.log("connecting");
 
 const RelayContext: Context<Relay> = createContext<Relay>(relay);
 
 const RelayProvider: Component<{ children: JSX.Element }> = (props) => {
-  return (
-    <RelayContext.Provider value={relay}>
-      {props.children}
-    </RelayContext.Provider>
-  );
+  return <RelayContext.Provider value={relay}>{props.children}</RelayContext.Provider>;
 };
 
 export { RelayProvider, RelayContext };
