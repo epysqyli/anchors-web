43bc17a76b6a85f1b38c67af5f38c5a2f2b3e4aa
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-23 00:02:10 +0200

    menu: update item style.

--- src/components/shared/Menu.tsx
+++ src/components/shared/Menu.tsx
@@ -4,7 +4,7 @@ import { CgFeed } from "solid-icons/cg";
 import { FiAnchor, FiEdit } from "solid-icons/fi";
 import { IoSettingsOutline } from "solid-icons/io";
 import { useLocation } from "solid-start";
-import { BsStack, BsSearch } from 'solid-icons/bs'
+import { BsStack, BsSearch } from "solid-icons/bs";
 
 interface Props {
   isNarrow: boolean | undefined;
@@ -23,54 +23,55 @@ const Menu: Component<Props> = (props) => {
 
   const actionStyle = `text-slate-50 w-5/6 mx-auto my-3 select-none
                        bg-gradient-to-r from-slate-800 to-gray-700
+                       md:bg-none md:px-5 md:py-8 md:rounded-none
                        hover:bg-slate-600 p-4 rounded-md text-center lg:text-left
                        transition cursor-pointer group active:scale-90
                        hover:text-orange-200 flex items-center justify-between
                        border-transparent border-2`;
 
-  const selectedActionStyle = actionStyle + " border-orange-200";
+  const selectedActionStyle = actionStyle + " md:border-y-orange-200 md:bg-slate-700";
   const active = (path: string) => path == location.pathname;
 
   return (
     <>
       <Show when={props.isNarrow !== undefined}>
         <div class={props.isNarrow ? narrowStyle : wideStyle}>
-          <div class="text-slate-100 w-fit mx-auto mt-10 mb-10 md:mb-20 ">
+          <div class='text-slate-100 w-fit mx-auto mt-10 mb-10 md:mb-20 '>
             <FiAnchor size={40} />
           </div>
 
-          <A onClick={props.toggleMenu} href="/">
+          <A onClick={props.toggleMenu} href='/'>
             <div class={active("/") ? selectedActionStyle : actionStyle}>
-              <CgFeed size={26} />
-              <div class="group-hover:scale-95">feed</div>
+              <CgFeed size={26} class='md:group-hover:animate-pulse' />
+              <div class='group-hover:scale-95'>feed</div>
             </div>
           </A>
 
-          <A onClick={props.toggleMenu} href="/write">
+          <A onClick={props.toggleMenu} href='/write'>
             <div class={active("/write") ? selectedActionStyle : actionStyle}>
-              <FiEdit size={26} />
-              <div class="group-hover:scale-95">write</div>
+              <FiEdit size={26} class='md:group-hover:animate-pulse' />
+              <div class='group-hover:scale-95'>write</div>
             </div>
           </A>
 
-          <A onClick={props.toggleMenu} href="/search">
+          <A onClick={props.toggleMenu} href='/search'>
             <div class={active("/search") ? selectedActionStyle : actionStyle}>
-              <BsSearch size={26} />
-              <div class="group-hover:scale-95">search</div>
+              <BsSearch size={26} class='md:group-hover:animate-pulse' />
+              <div class='group-hover:scale-95'>search</div>
             </div>
           </A>
 
-          <A onClick={props.toggleMenu} href="/my-posts">
+          <A onClick={props.toggleMenu} href='/my-posts'>
             <div class={active("/my-posts") ? selectedActionStyle : actionStyle}>
-              <BsStack size={26} />
-              <div class="group-hover:scale-95">my posts</div>
+              <BsStack size={26} class='md:group-hover:animate-pulse' />
+              <div class='group-hover:scale-95'>my posts</div>
             </div>
           </A>
 
-          <A onClick={props.toggleMenu} href="/settings">
+          <A onClick={props.toggleMenu} href='/settings'>
             <div class={active("/settings") ? selectedActionStyle : actionStyle}>
-              <IoSettingsOutline size={26} />
-              <div class="group-hover:scale-95">settings</div>
+              <IoSettingsOutline size={26} class='md:group-hover:animate-pulse' />
+              <div class='group-hover:scale-95'>settings</div>
             </div>
           </A>
         </div>
