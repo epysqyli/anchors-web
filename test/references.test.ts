7a8e77d436ce2181d912afe218892d581a3f2181
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-06-27 23:26:42 +0200

    references-test: format.

--- test/references.test.ts
+++ test/references.test.ts
@@ -2,17 +2,9 @@ import { expect, test } from "vitest";
 import { parseReferenceType } from "~/lib/references";
 
 test("parseReferenceType parses urls into correct RefTagCategory", () => {
-  expect(parseReferenceType("https://github.com/solidjs/solid-testing-library")).toEqual("generic");
-
-  expect(
-    parseReferenceType(
-      "https://www.youtube.com/watch?v=ZyQLlWzOvYw&list=FL6zRqV5BoLaPshnUjI_oLPg&index=1&t=22s"
-    )
-  ).toEqual("video");
-
-  expect(parseReferenceType("https://openlibrary.org/works/OL62181W")).toEqual("book");
-
   expect(parseReferenceType("https://www.themoviedb.org/movie/311")).toEqual("movie");
-
+  expect(parseReferenceType("https://openlibrary.org/works/OL62181W")).toEqual("book");
+  expect(parseReferenceType("https://www.youtube.com/watch?v=ZyQLlWzOvYw&list")).toEqual("video");
+  expect(parseReferenceType("https://github.com/solidjs/solid-testing-library")).toEqual("generic");
   expect(parseReferenceType("https://open.spotify.com/track/71N78jUmfhLeRDlLd8elfl")).toEqual("song");
 });
