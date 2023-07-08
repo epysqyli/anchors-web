c597206b30b140403133497c002f29563a2e1730
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-07-08 18:08:33 +0200

    event-wrapper: refactor events sub and user metadata enrichment.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -8,41 +8,42 @@ import { Event, Filter, Kind, Sub, validateEvent, verifySignature } from "nostr-
 
 const Home: Component<{}> = () => {
   const [events, setEvents] = createSignal<IEnrichedEvent[]>([]);
+  const [enrichedEvents, setEnrichedEvents] = createSignal<IEnrichedEvent[]>([]);
   const relay = useContext(RelayContext);
   const [eventWrapperContainer, setEventWrapperContainer] = createSignal<HTMLDivElement>();
 
-  // createServerData$ until eose ?
+  // TODO: manage incoming events after EOSE
   onMount(async () => {
-    const filter: Filter = {};
-    const sub: Sub = relay.sub([filter]);
+    const basicEventsSub: Sub = relay.sub([{}]);
 
-    sub.on("eose", () => {
-      // sub.unsub();
+    basicEventsSub.on("event", (nostrEvent) => {
+      const currentEvent = nostrEvent as Event;
+      if (currentEvent.kind === Kind.Text && validateEvent(nostrEvent) && verifySignature(nostrEvent)) {
+        setEvents([...events(), { ...currentEvent, name: "", picture: "", about: "" }]);
+      }
     });
 
-    sub.on("event", (nostrEvent) => {
-      const currentEvent = nostrEvent as Event;
+    basicEventsSub.on("eose", () => {
+      const metadataFilter: Filter = {
+        authors: [...new Set(events().map((e) => e.pubkey))],
+        kinds: [Kind.Metadata]
+      };
 
-      if (currentEvent.kind === Kind.Text && validateEvent(nostrEvent) && verifySignature(nostrEvent)) {
-        const metaDataFilter: Filter = {
-          authors: [currentEvent.pubkey],
-          kinds: [Kind.Metadata]
-        };
+      const metadataSub: Sub = relay.sub([metadataFilter]);
 
-        let userMetadata: IUserMetadata;
-        const metadataSub: Sub = relay.sub([metaDataFilter]);
+      metadataSub.on("event", (metadataEvent) => {
+        const userMetadata: IUserMetadata = JSON.parse(metadataEvent.content);
+        const eventsToEnrich = events().filter((ev) => ev.pubkey === metadataEvent.pubkey);
 
-        metadataSub.on("event", (metadataEvent) => {
-          userMetadata = JSON.parse(metadataEvent.content);
+        const newEnrichedEvents: IEnrichedEvent[] = eventsToEnrich.map((ev) => {
+          ev.about = userMetadata.about;
+          ev.name = userMetadata.name;
+          ev.picture = userMetadata.picture;
+          return ev;
+        })
 
-          const enrichedEvent: IEnrichedEvent = {
-            ...currentEvent,
-            ...userMetadata
-          };
-
-          setEvents([...events(), enrichedEvent]);
-        });
-      }
+        setEnrichedEvents([...enrichedEvents(), ...newEnrichedEvents]);
+      })
     });
   });
 
@@ -57,7 +58,7 @@ const Home: Component<{}> = () => {
     <>
       <Show when={useIsNarrow() !== undefined && useIsNarrow()}>
         <div class='snap-y snap-mandatory overflow-scroll overflow-x-hidden h-[100vh]'>
-          <For each={events()}>
+          <For each={enrichedEvents()}>
             {(nostrEvent) => <EventWrapper event={nostrEvent} isNarrow={useIsNarrow()} />}
           </For>
         </div>
@@ -68,7 +69,7 @@ const Home: Component<{}> = () => {
           ref={(el) => setEventWrapperContainer(el)}
           class='custom-scrollbar snap-y snap-mandatory overflow-scroll overflow-x-hidden h-full'
         >
-          <For each={events()}>
+          <For each={enrichedEvents()}>
             {(nostrEvent) => (
               <EventWrapper event={nostrEvent} isNarrow={useIsNarrow()} scrollPage={scrollPage} />
             )}
