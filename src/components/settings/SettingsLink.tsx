d7674b404b61c6b2e509ea0dc7404cb5d2e3a5f5
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-07-15 20:39:09 +0200

    settings: update style.

--- src/components/settings/SettingsLink.tsx
+++ src/components/settings/SettingsLink.tsx
@@ -1,6 +1,5 @@
 import { A } from "@solidjs/router";
 import { Component, JSX } from "solid-js";
-import { VsLinkExternal } from "solid-icons/vs";
 
 interface Props {
   href: string;
@@ -11,14 +10,8 @@ interface Props {
 const SettingsLink: Component<Props> = (props): JSX.Element => {
   return (
     <A href={props.href}>
-      <div class='h-full text-slate-200 group bg-neutral-600 rounded border-neutral-600 text-lg'>
-        <div class='flex items-center justify-center h-3/4'>
-          <div class='grow text-slate-300'>{props.children}</div>
-
-          <div class='self-start p-5 mx-auto group-hover:text-slate-200 transition-all text-slate-400'>
-            <VsLinkExternal size={24} />
-          </div>
-        </div>
+      <div class='h-full group text-slate-200 group bg-neutral-600 rounded border-neutral-600 text-lg'>
+        <div class='h-3/4 py-10 mx-auto text-slate-300 group-hover:text-orange-200'>{props.children}</div>
         <div class='h-1/4 text-center bg-neutral-700 py-3 group-active:bg-neutral-800'>{props.content}</div>
       </div>
     </A>
