24ae79a37f03a38248dbaa73c1769b6cf2cc147f
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-07-15 20:03:35 +0200

    action-popup: create component.

---
+++ src/components/shared/ActionPopup.tsx
@@ -0,0 +1,9 @@
+// component that pops up on succes or fail
+
+import { Component } from "solid-js";
+
+const ActionPopup: Component<{ message: string }> = (props) => {
+  return <></>;
+};
+
+export default ActionPopup;
