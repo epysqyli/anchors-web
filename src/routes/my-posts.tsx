180f1ed7ec05f535b37c619290bdcad7633c95c2
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-07-22 18:03:47 +0200

    my-posts: add basic fetch logic.

--- src/routes/my-posts.tsx
+++ src/routes/my-posts.tsx
@@ -1,9 +1,37 @@
-import { Component } from "solid-js";
+import { RelayContext } from "~/contexts/relay";
+import IEnrichedEvent from "~/interfaces/IEnrichedEvent";
+import { sortByCreatedAt } from "~/lib/nostr/nostr-utils";
+import { Event, Kind, Sub, validateEvent, verifySignature } from "nostr-tools";
+import { Show, VoidComponent, createSignal, onMount, useContext } from "solid-js";
+import LoadingFallback from "~/components/feed/LoadingFallback";
 
-const MyPosts: Component<{}> = (props) => {
+const MyPosts: VoidComponent = () => {
+  const relay = useContext(RelayContext);
+  const [isLoading, setIsLoading] = createSignal<boolean>(true);
+  const [events, setEvents] = createSignal<IEnrichedEvent[]>([]);
+
+  onMount(async () => {
+    const eventsSub: Sub = relay.sub([
+      { authors: ["8becb32986f141b2399559e34fd31a720376f4bbbeb735ed8a3288d544cf946f"] }
+    ]);
+
+    eventsSub.on("event", (nostrEvent: Event) => {
+      if (nostrEvent.kind === Kind.Text && validateEvent(nostrEvent) && verifySignature(nostrEvent)) {
+        setEvents([...events(), { ...nostrEvent, name: "", picture: "", about: "" }].sort(sortByCreatedAt));
+      }
+    });
+
+    eventsSub.on("eose", () => {
+      setIsLoading(false);
+    });
+  });
+
   return (
     <>
       <h1 class='text-slate-100 text-center text-2xl md:text-4xl font-bold my-14'>Your nostr posts</h1>
+      <Show when={!isLoading()} fallback={<LoadingFallback />}>
+        <div class='text-center text-slate-50'>{events()[0].content}</div>
+      </Show>
     </>
   );
 };
--- test/pages/MyPosts.test.tsx
+++ test/pages/MyPosts.test.tsx
@@ -1,12 +1,20 @@
-import { render } from "@solidjs/testing-library";
-import { describe } from "vitest";
+import { describe, it } from "vitest";
 import MyPosts from "~/routes/my-posts";
+import { render } from "@solidjs/testing-library";
 
 describe("<MyPosts />", () => {
   it("shows the correct title", async () => {
     const { unmount, getByText } = render(() => <MyPosts />);
     const title = getByText("Your nostr posts") as HTMLHeadingElement;
     expect(title).toBeInTheDocument();
+
+    unmount();
+  });
+
+  it.skip("shows all fetched events", async () => {
+    const { unmount, findByText } = render(() => <MyPosts />);
+
+    expect(await findByText("Just some new event") as HTMLDivElement).toBeInTheDocument();
 
     unmount();
   });
