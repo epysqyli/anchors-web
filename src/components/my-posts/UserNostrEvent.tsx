d9a2c52426baeb382d8608e75b6dc3f1452fa3d9
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-07-23 16:43:45 +0200

    user-nostr-event: add link to id events page.

--- src/components/my-posts/UserNostrEvent.tsx
+++ src/components/my-posts/UserNostrEvent.tsx
@@ -1,3 +1,4 @@
+import { A } from "@solidjs/router";
 import { Event } from "nostr-tools";
 import { Component, JSX } from "solid-js";
 import { parseDate, shrinkContent } from "~/lib/nostr/nostr-utils";
@@ -8,7 +9,13 @@ const UserNostrEvent: Component<{ nostrEvent: Event }> = (props): JSX.Element =>
       <div class='bg-slate-600 text-center rounded-md mx-auto w-3/4 break-words'>
         {parseDate(props.nostrEvent.created_at)}
       </div>
-      <div class='text-sm my-5 text-neutral-400 break-all'>{props.nostrEvent.id}</div>
+      <A
+        href={`/events/${props.nostrEvent.id}`}
+        class='text-sm my-5 text-neutral-400 break-all bg-neutral-600
+               hover:text-neutral-200 active:scale-95 p-2 rounded-md transition-all'
+      >
+        {props.nostrEvent.id}
+      </A>
       <div class='break-all'>{shrinkContent(props.nostrEvent.content)}</div>
       <div class='flex items-center justify-around w-4/5 mx-auto mt-5 pt-4 text-neutral-400 transition-all'>
         <span>reactions</span>
