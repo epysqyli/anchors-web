0f7d01502f268807a13b15d2f61e442efea8c446
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-07-23 16:09:32 +0200

    my-posts: update break words style and xl cols.

--- src/components/my-posts/UserNostrEvent.tsx
+++ src/components/my-posts/UserNostrEvent.tsx
@@ -6,12 +6,12 @@ import { parseDate, shrinkContent } from "~/lib/nostr/nostr-utils";
 
 const UserNostrEvent: Component<{ nostrEvent: Event }> = (props): JSX.Element => {
   return (
-    <div class='shadow-xl break-all py-5 px-5 text-slate-300 bg-neutral-700 bg-opacity-75 rounded-md hover:text-slate-200'>
-      <div class='bg-slate-600 text-center rounded-md mx-auto w-3/4'>
+    <div class='shadow-xl py-5 px-5 text-slate-300 bg-neutral-700 bg-opacity-75 rounded-md hover:text-slate-200'>
+      <div class='bg-slate-600 text-center rounded-md mx-auto w-3/4 break-words'>
         {parseDate(props.nostrEvent.created_at)}
       </div>
-      <div class='text-sm my-5 text-neutral-400'>{props.nostrEvent.id}</div>
-      <div>{shrinkContent(props.nostrEvent.content)}</div>
+      <div class='text-sm my-5 text-neutral-400 break-all'>{props.nostrEvent.id}</div>
+      <div class="break-all">{shrinkContent(props.nostrEvent.content)}</div>
       <div
         class='flex items-center justify-around w-4/5 mx-auto border-t border-opacity-50
                  border-neutral-100 mt-5 pt-4 text-neutral-400 transition-all'
