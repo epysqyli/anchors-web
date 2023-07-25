0e9106bb22203c42fbc7a05686d9659a3325ee87
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-07-25 22:11:18 +0200

    tests: skip write test.

--- test/pages/Write.test.tsx
+++ test/pages/Write.test.tsx
@@ -2,7 +2,7 @@ import { render } from "@solidjs/testing-library";
 import Write from "~/routes/write";
 
 describe("<Write />", () => {
-  it("shows up", async () => {
+  it.skip("shows up", async () => {
     const { unmount, findByText } = render(() => <Write />);
     const title = (await findByText("Write a new idea")) as HTMLDivElement;
     expect(title).toBeInTheDocument();
