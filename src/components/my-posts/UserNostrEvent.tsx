8e161fb6c3caa3f00d97aa7039479174180e9faf
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-07-26 22:21:26 +0200

    user-nostr-event: update style and add remove button.

--- src/components/my-posts/UserNostrEvent.tsx
+++ src/components/my-posts/UserNostrEvent.tsx
@@ -1,5 +1,6 @@
 import { A } from "@solidjs/router";
 import { Event } from "nostr-tools";
+import { CgRemove } from "solid-icons/cg";
 import { Component, JSX } from "solid-js";
 import { parseDate, shrinkContent } from "~/lib/nostr/nostr-utils";
 
@@ -9,18 +10,20 @@ const UserNostrEvent: Component<{ nostrEvent: Event }> = (props): JSX.Element =>
       <div class='bg-slate-600 text-center rounded-md mx-auto w-3/4 break-words'>
         {parseDate(props.nostrEvent.created_at)}
       </div>
+
+      <div class='break-all my-10'>{shrinkContent(props.nostrEvent.content)}</div>
       <A
         href={`/events/${props.nostrEvent.id}`}
-        class='text-sm my-5 text-neutral-400 break-all bg-neutral-600
-               hover:text-neutral-200 active:scale-95 p-2 rounded-md transition-all'
+        class='text-sm text-slate-400 break-all bg-slate-600
+               hover:text-slate-200 active:scale-95 p-2 rounded-md transition-all'
       >
         {props.nostrEvent.id}
       </A>
-      <div class='break-all'>{shrinkContent(props.nostrEvent.content)}</div>
-      <div class='flex items-center justify-around w-4/5 mx-auto mt-5 pt-4 text-neutral-400 transition-all'>
-        <span>reactions</span>
-        <span>comments</span>
-        <span>delete</span>
+      <div class='w-4/5 mx-auto mt-10 text-neutral-400 cursor-pointer group'>
+        <CgRemove
+          size={32}
+          class='transition-all mx-auto group-hover:scale-105 group-hover:text-neutral-200 group-active:scale-90'
+        />
       </div>
     </div>
   );
