d44484ae87c513b88d5e48bf73e8c67dae43dfcc
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-08-23 12:16:51 +0200

    settings: add manage following card.

--- src/components/settings/SettingsLink.tsx
+++ src/components/settings/SettingsLink.tsx
@@ -11,8 +11,8 @@ const SettingsLink: Component<Props> = (props): JSX.Element => {
   return (
     <A href={props.href}>
       <div class='h-full group text-slate-200 group bg-neutral-500 bg-opacity-70 rounded-md border-neutral-600 text-lg'>
-        <div class='h-3/4 py-10 mx-auto text-slate-300 group-hover:text-slate-50'>{props.children}</div>
-        <div class='h-1/4 text-center bg-neutral-700 py-3 group-active:bg-neutral-800'>{props.content}</div>
+        <div class='h-3/5 py-10 mx-auto text-slate-300 group-hover:text-slate-50'>{props.children}</div>
+        <div class='h-2/5 text-center bg-neutral-700 py-3 group-active:bg-neutral-800'>{props.content}</div>
       </div>
     </A>
   );
--- src/routes/settings/index.tsx
+++ src/routes/settings/index.tsx
@@ -1,4 +1,5 @@
 import { VoidComponent } from "solid-js";
+import { TbUsersGroup } from "solid-icons/tb";
 import { BsLightningChargeFill } from "solid-icons/bs";
 import SettingsLink from "~/components/settings/SettingsLink";
 import { RiBusinessProfileLine, RiMapSignalTowerFill } from "solid-icons/ri";
@@ -9,13 +10,18 @@ const Settings: VoidComponent = () => {
       <h1 class='text-slate-100 text-center text-2xl md:text-4xl font-bold mt-14'>
         Manage your nostr profile
       </h1>
-      <div class='grid grid-cols-2 w-2/3 h-1/2 mx-auto gap-10 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>
+
+      <div class='grid grid-cols-3 w-4/5 h-3/5 mx-auto px-5 gap-5 mt-20 overflow-y-scroll custom-scrollbar'>
         <SettingsLink href='user-metadata' content='set or update your profile info'>
           <RiBusinessProfileLine class='mx-auto' size={62} />
         </SettingsLink>
 
         <SettingsLink href='/' content='manage relays'>
           <RiMapSignalTowerFill class='mx-auto' size={62} />
+        </SettingsLink>
+
+        <SettingsLink href='/' content='manage your following'>
+          <TbUsersGroup class='mx-auto' size={62} />
         </SettingsLink>
 
         <SettingsLink href='/' content='set your lightning address?'>
