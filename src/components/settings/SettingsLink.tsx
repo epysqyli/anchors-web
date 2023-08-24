11bb8a1d0f553359a39f721be8c1ca98e8adeeee
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-08-24 14:17:00 +0200

    settings-link: change bg colors.

--- src/components/settings/SettingsLink.tsx
+++ src/components/settings/SettingsLink.tsx
@@ -10,9 +10,9 @@ interface Props {
 const SettingsLink: Component<Props> = (props): JSX.Element => {
   return (
     <A href={props.href}>
-      <div class='h-full group text-slate-200 group bg-neutral-500 bg-opacity-70 rounded-md border-neutral-600 text-lg'>
+      <div class='h-full group text-slate-200 group bg-slate-600 bg-opacity-70 rounded-md border-neutral-600 text-lg'>
         <div class='h-3/5 py-10 mx-auto text-slate-300 group-hover:text-slate-50'>{props.children}</div>
-        <div class='h-2/5 text-center bg-neutral-700 group-active:bg-neutral-800 relative'>
+        <div class='h-2/5 text-center bg-neutral-600 group-active:bg-neutral-800 relative'>
           <div class='absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2'>{props.content}</div>
         </div>
       </div>
