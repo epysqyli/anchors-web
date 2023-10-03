d0df4fef51de5245ccbe11c25dcf23996ce925f6
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-10-03 23:07:47 +0200

    event-anchor: update bg style.

--- src/components/feed/EventAnchor.tsx
+++ src/components/feed/EventAnchor.tsx
@@ -9,10 +9,10 @@ interface Props {
 const EventAnchor: Component<Props> = (props): JSX.Element => {
   const anchorNostrIDstyle = (): string => {
     if (useLocation().pathname.includes(props.nostrEventID)) {
-      return "text-sm break-all w-1/6 text-slate-400 bg-slate-600 px-2 py-1 rounded cursor-default";
+      return "text-sm break-all w-1/6 text-slate-400 bg-slate-700 px-2 py-1 rounded cursor-default";
     }
 
-    return `text-sm break-all w-1/5 text-slate-400 cursor-pointer bg-slate-600
+    return `text-sm break-all w-1/5 text-slate-400 cursor-pointer bg-slate-700
             px-2 py-1 rounded hover:text-slate-200 active:scale-95 transition-all`;
   };
 
