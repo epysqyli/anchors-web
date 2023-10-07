f8c778e276c3a905277823bc6792f4dc9c4b28a1
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-10-07 12:23:11 +0200

    anchors-mode-selector: show current selection at all times.

--- src/components/Menu/AnchorsModeSelector.tsx
+++ src/components/Menu/AnchorsModeSelector.tsx
@@ -1,20 +1,28 @@
 import { Motion } from "@motionone/solid";
 import { BsGlobe2 } from "solid-icons/bs";
 import { FiAnchor } from "solid-icons/fi";
-import { JSX, VoidComponent, useContext } from "solid-js";
+import { JSX, VoidComponent, createSignal, useContext } from "solid-js";
 import { RelayContext } from "~/contexts/relay";
 
 const AnchorsModeSelector: VoidComponent = (): JSX.Element => {
   const { isAnchorsMode, setIsAnchorsMode } = useContext(RelayContext);
 
   const toggleAnchorsMode = (): void => {
+    toggleSwitchMsg();
     setIsAnchorsMode(!isAnchorsMode());
+  };
+
+  const [showSwitchMsg, setShowSwitchMsg] = createSignal<boolean>(false);
+  const toggleSwitchMsg = (): void => {
+    setShowSwitchMsg(!showSwitchMsg());
   };
 
   return (
     <>
       <div
         onClick={toggleAnchorsMode}
+        onMouseEnter={toggleSwitchMsg}
+        onMouseLeave={toggleSwitchMsg}
         class='text-slate-100 w-fit mx-auto cursor-pointer transition
                    border-2 border-dashed border-neutral-500 hover:bg-neutral-500
                    hover:border-neutral-600 rounded-full p-3 active:bg-neutral-700'
@@ -30,11 +38,12 @@ const AnchorsModeSelector: VoidComponent = (): JSX.Element => {
         )}
       </div>
 
-      <div
-        class='mt-3 rounded px-2 py-1 text-sm bg-transparent text-transparent select-none
-                      group-hover:text-neutral-300 group-hover:bg-neutral-600 transition'
-      >
-        switch to {isAnchorsMode() ? "all nostr" : "only anchors"} posts
+      <div class='mt-3 rounded px-2 py-1 text-sm select-none text-neutral-400 bg-neutral-700 transition'>
+        {showSwitchMsg() ? (
+          <span>switch to {isAnchorsMode() ? "all nostr" : "only anchors"} posts</span>
+        ) : (
+          <span class=''>currently showing {isAnchorsMode() ? "anchors" : "all"} posts</span>
+        )}
       </div>
     </>
   );
