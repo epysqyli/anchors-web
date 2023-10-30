7894893442759579d4e74d6b8ce5f1906e1191eb
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-10-30 22:20:43 +0100

    relay-ctx: move interface declaration just before use.

--- src/contexts/relay.tsx
+++ src/contexts/relay.tsx
@@ -1,14 +1,8 @@
 import "websocket-polyfill";
-import type { Accessor, Context, Setter } from "solid-js";
 import Relayer from "~/lib/nostr/relayer";
-import { Component, JSX, createContext, createSignal } from "solid-js";
+import type { Accessor, Context, Setter } from "solid-js";
 import { getPublicKeyFromExt } from "~/lib/nostr/nostr-utils";
-
-interface IRelayContext {
-  relay: Relayer;
-  readRelays: { get: Accessor<string[]>; set: Setter<string[]> };
-  anchorsMode: { get: Accessor<boolean>; set: Setter<boolean> };
-}
+import { Component, JSX, createContext, createSignal } from "solid-js";
 
 const [getReadRelays, setReadRelays] = createSignal<string[]>([]);
 const [getAnchorsMode, setAnchorsMode] = createSignal<boolean>(true);
@@ -21,6 +15,12 @@ if (pk) {
   await relay.fetchAndSetRelays();
   setReadRelays(relay.getReadRelays());
   relay.following = await relay.fetchContacts();
+}
+
+interface IRelayContext {
+  relay: Relayer;
+  readRelays: { get: Accessor<string[]>; set: Setter<string[]> };
+  anchorsMode: { get: Accessor<boolean>; set: Setter<boolean> };
 }
 
 const RelayContext: Context<IRelayContext> = createContext({
