52fb5c7bae709fa3d1f909eb6210b16417d82002
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-11-03 22:51:21 +0100

    event-content: remove text justify.

--- src/components/feed/EventContent.tsx
+++ src/components/feed/EventContent.tsx
@@ -4,7 +4,7 @@ const EventContent: Component<{ content: string }> = (props): JSX.Element => {
   return (
     <div class='col-span-4 xl:col-span-4 bg-neutral-900 bg-opacity-30 rounded-md h-full overflow-y-hidden py-10'>
       <p
-        class='w-5/6 px-5 mx-auto text-slate-300 tracking-tighter break-words text-justify
+        class='w-5/6 px-5 mx-auto text-slate-300 tracking-tighter break-words
                whitespace-pre-line overflow-y-scroll custom-scrollbar h-full'
       >
         {props.content}
