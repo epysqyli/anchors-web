532c7211173d81f9c42b350fe396feabf817064b
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-11-03 23:09:19 +0100

    loading-points: change points numbers.

--- src/components/feed/LoadingPoints.tsx
+++ src/components/feed/LoadingPoints.tsx
@@ -3,10 +3,10 @@ import { VoidComponent } from "solid-js";
 const LoadingPoints: VoidComponent = () => {
   return (
     <div class='animate-pulse text-slate-200 absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 w-1/2'>
-      {Array.from({ length: 15 }).map((_) => {
+      {Array.from({ length: 13 }).map((_) => {
         return (
           <div class='flex justify-around'>
-            {Array.from({ length: 10 }).map((_) => {
+            {Array.from({ length: 20 }).map((_) => {
               return <div class='w-1 h-1 rounded-full bg-white my-4'></div>;
             })}
           </div>
