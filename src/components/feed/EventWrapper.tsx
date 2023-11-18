3eec866e0d5cd85241b7ead2a2b6771203ee0aae
elvis.pysqyli <elvis.pysqyli@gmail.com>
2023-11-18 14:18:41 +0100

    event-wrapper: fix date display on mobile.

--- src/components/feed/EventWrapper.tsx
+++ src/components/feed/EventWrapper.tsx
@@ -174,7 +174,10 @@ const EventWrapper: Component<Props> = (props) => {
           </div>
 
           <div class='h-[10%] flex items-center justify-around gap-x-3'>
-            <div class='text-sm text-slate-400 w-1/3 text-center'>{parseDate(props.event.created_at)}</div>
+            <div class='text-sm text-slate-400 w-1/3 text-center'>
+              <p>{new Date(props.event.created_at).toTimeString().split(" ")[0]}</p>
+              <p>{new Date(props.event.created_at).toDateString()}</p>
+            </div>
             <EventAnchor nostrEventID={props.event.id} />
             {authMode.get() == "private" ? <RepostAction enrichedEvent={props.event} /> : <></>}
           </div>
